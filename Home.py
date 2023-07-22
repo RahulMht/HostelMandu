b70afddd00f4adfae3eaa7924f608c167ab158b3
Rahul Ravi Mahatha <135226419+RahulMht@users.noreply.github.com>
2023-07-22 23:58:42 +0545

    last change to fix eror

--- Home.py
+++ Home.py
@@ -50,7 +50,7 @@ auth_pyrebase = firebase.auth()
 
 # Initialize Firebase with Admin SDK
 cred = credentials.Certificate("pages/campusday-aebdc-94e7767c0207.json")
-firebase_admin.initialize_app(cred)
+#firebase_admin.initialize_app(cred)
 auth_admin = auth
 
 def login():
